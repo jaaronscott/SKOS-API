852e5d4aadd9c975e8fe627e6e4ad53aafe88849
jaaronscott <jaaronscott@gmail.com>
2025-05-09 15:25:49 -0700

    Create app.js

---
+++ Reflective-Core/Src/src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Mirrorborn from './components/Mirrorborn';
+import Eitherion from './components/Eitherion';
+import Glyph from './components/Glyph';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Nordic Sweater UI</h1>
+      <Mirrorborn />
+      <Eitherion />
+      <Glyph />
+    </div>
+  );
+};
+
+export default App;
