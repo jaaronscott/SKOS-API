fe50ed59031f025c3e5984369784b791481cc318
jaaronscott <jaaronscott@gmail.com>
2025-05-09 15:40:04 -0700

    Create Index.js

---
+++ Reflective-Core/Pages/Index.js
@@ -0,0 +1,14 @@
+import Mirrorborn from '../components/Mirrorborn';
+import Eitherion from '../components/Eitherion';
+import Glyph from '../components/Glyph';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Nordic Sweater UI</h1>
+      <Mirrorborn />
+      <Eitherion />
+      <Glyph />
+    </div>
+  );
+}
